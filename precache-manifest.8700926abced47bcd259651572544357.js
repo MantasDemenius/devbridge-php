e8d37254853379f98059dc5331ab01f4da457267
Tidish <tidish@inbox.lt>
2019-03-10 21:49:19 +0200

    Updates

---
+++ precache-manifest.8700926abced47bcd259651572544357.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2e5323a9a7f2874fa35cf713c08df82",
+    "url": "/devbridge-php/static/media/bg.a2e5323a.jpg"
+  },
+  {
+    "revision": "208546ef538b7734f661",
+    "url": "/devbridge-php/static/js/runtime~main.208546ef.js"
+  },
+  {
+    "revision": "9512b5a744840319884b",
+    "url": "/devbridge-php/static/js/main.9512b5a7.chunk.js"
+  },
+  {
+    "revision": "00f8d224d99d311c6e97",
+    "url": "/devbridge-php/static/js/2.00f8d224.chunk.js"
+  },
+  {
+    "revision": "9512b5a744840319884b",
+    "url": "/devbridge-php/static/css/main.3409f886.chunk.css"
+  },
+  {
+    "revision": "0ff6a8cb41fdad439631a12021c9fcd1",
+    "url": "/devbridge-php/index.html"
+  }
+];
